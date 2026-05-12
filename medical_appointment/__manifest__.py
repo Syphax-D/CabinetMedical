f094e5a4dbc80601b9f4b5a53d1e12c7f92e051e
Said <said.amroun@etu.univ-amu.fr>
2026-05-12 16:56:34 +0200

    Ajout de la possibilite d'impression d'ordonnace, vue calendrier pour les rdv, envoie mail de confirmation de rdv au patient(ca fonctionne pas a 100%)

--- medical_appointment/__manifest__.py
+++ medical_appointment/__manifest__.py
@@ -5,13 +5,15 @@
     'description': 'Schedule and manage appointments between patients and doctors.',
     'author': 'DIB Syphax / AMROUN Said',
     'category': 'Healthcare',
-    'depends': ['base', 'medical_base'],
+    'depends': ['base', 'mail', 'medical_base'],
     'data': [
         'security/security_rules.xml',
         'security/ir.model.access.csv',
         'data/sequence.xml',
+        'data/mail_template.xml',
         'views/dashboard_action.xml',
         'views/appointment_views.xml',
+        'views/patient_views.xml',
         'views/medical_appointment_menu.xml',
     ],
     'assets': {
